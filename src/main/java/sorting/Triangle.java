45c4cc0b7e5dce452654f2b007674fe4591b34a4
homerb <ben.homer@thehutgroup.com>
2016-02-02 22:02:49 +0000

    Triangle

---
+++ src/main/java/sorting/Triangle.java
@@ -0,0 +1,37 @@
+package sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Triangle {
+
+    public int solution(int[] A) {
+        Arrays.sort(A);
+
+        double a, b, c;
+        for (int i = 0; i < A.length-2; i++) {
+            a = A[i];
+            b = A[i+1];
+            c = A[i+2];
+            if (a + b > c &&
+                a + c > b &&
+                b + c > a) {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+
+    @Test
+    public void test() {
+        final int[] A = { 10, 2, 5, 1, 8, 20 };
+        Assert.assertEquals(1, solution(A));
+
+        final int[] B = { 10, 50, 5, 1 };
+        Assert.assertEquals(1, solution(A));
+    }
+}
