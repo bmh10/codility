ed5362d7ebd68669d387a9fcd8cb93297376d968
bmh10 <bmhomer13@gmail.com>
2016-02-10 21:03:25 +0000

    Fish

--- src/main/java/stacksAndQueues/Fish.java
+++ src/main/java/stacksAndQueues/Fish.java
@@ -5,14 +5,39 @@ import org.junit.Test;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Fish {
 
+    // Time O(N) Space O(N)
     public int solution(int[] A, int[] B) {
-        // TODO
-        return -1;
+        final int N = A.length;
+        final Deque<Integer> survivors = new ArrayDeque<Integer>();
+
+        for (int i = 0; i < N; i++) {
+            if (survivors.isEmpty() || B[i] == 1) {
+                survivors.push(i);
+                continue;
+            }
+
+            // B[i] == 0 now
+            Integer prevIdx = survivors.peek();
+            boolean currentFishWins = true;
+            while (prevIdx != null && B[prevIdx] == 1 && currentFishWins) {
+                currentFishWins = false;
+                if (A[i] > A[prevIdx]) {
+                    survivors.pop();
+                    currentFishWins = true;
+                }
+
+                prevIdx = survivors.peek();
+            }
+
+            if (currentFishWins) {
+                survivors.push(i);
+            }
+        }
+
+        return survivors.size();
     }
 
     @Test
