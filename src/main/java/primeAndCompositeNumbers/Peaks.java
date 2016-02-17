d4b87353bc8819a8e612fdb27bd00daf76251ea3
bmh10 <bmhomer13@gmail.com>
2016-02-17 21:28:54 +0000

    Peaks 100%

--- src/main/java/primeAndCompositeNumbers/Peaks.java
+++ src/main/java/primeAndCompositeNumbers/Peaks.java
@@ -4,18 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Peaks {
 
-    //
+    // Time O(N*log(log(N)) Space O(N)
     public int solution(int A[]) {
         final int N = A.length;
-        final double sqrtN = Math.sqrt(N);
-
         final List<Integer> peaks = new ArrayList<Integer>();
 
-        // O(N)
         for (int i = 1; i < N - 1; i++) {
             if (A[i] > A[i-1] && A[i] > A[i+1]) {
                 peaks.add(i);
@@ -26,48 +25,24 @@ public class Peaks {
             return 0;
         }
 
-        int max = 0;
-        for (int i = peaks.get(0); i <= sqrtN; i++) {
-            if (N % i == 0) {
-                if (isCorrect(i, N, peaks)) {
-                    max = Math.max(max, i);
+        final int maxPossibleBlocks = peaks.size();
+
+        for (int blocks = maxPossibleBlocks; blocks > 0; blocks--) {
+            if (N % blocks == 0) {
+                final int blockSize = N / blocks;
+                final Set<Integer> completeBlocks = new HashSet<Integer>();
+                for (int peakIdx : peaks) {
+                    final int peakBlock = Math.floorDiv(peakIdx, blockSize);
+                    completeBlocks.add(peakBlock);
                 }
 
-                // Then try i's matching pair
-                int j = N / i;
-                if (j <= i) {
-                    continue;
-                }
-                if (isCorrect(j, N, peaks)) {
-                    max = Math.max(max, i);
+                if (completeBlocks.size() == blocks) {
+                    return blocks;
                 }
             }
         }
 
-        return max;
-    }
-
-    private boolean isCorrect(int gap, int N, List<Integer> peaks) {
-        int st = 0;
-        int end;
-        while (st < N) {
-            end = st + gap - 1;
-            boolean peakFound = false;
-            for (int p : peaks) {
-                if (p >= st && p <= end) {
-                    peakFound = true;
-                    break;
-                }
-            }
-
-            if (!peakFound) {
-                return false;
-            }
-
-            st += gap;
-        }
-
-        return true;
+        return 0;
     }
 
     @Test
