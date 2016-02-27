d4f01a95bcb8ac890079eb8d02b30d23cc491c64
bmh10 <bmhomer13@gmail.com>
2016-02-27 15:15:25 +0000

    MinAvgTwoSlice

--- src/main/java/prefixSums/MinAvgTwoSlice.java
+++ src/main/java/prefixSums/MinAvgTwoSlice.java
@@ -3,39 +3,33 @@ package prefixSums;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class MinAvgTwoSlice {
 
-    /*public int solution(int[] A) {
+    public int solution(int[] A) {
         final int N = A.length;
-        double[] avg = new double[N];
-        avg[N-1] = A[N-1];
-        for (int i = N-2; i >= 0; i--) {
-            avg[i] = A[i] + avg[i+1];
-        }
 
-        for (int i = 0; i < N; i++) {
-            avg[i] /= (N - i);
-        }
-
-        double minVal = Double.MAX_VALUE;
+        double minAvg = Double.MAX_VALUE;
         int minIdx = -1;
-        for (int i = 0; i < N; i++) {
-            if (avg[i] < minVal) {
-                minVal = avg[i];
+        for (int i = 0; i < N-2; i++) {
+            double avg2 = (A[i] + A[i+1]) / 2d;
+            if (avg2 < minAvg) {
+                minAvg = avg2;
+                minIdx = i;
+            }
+
+            double avg3 = (A[i] + A[i+1] + A[i+2]) / 3d;
+            if (avg3 < minAvg) {
+                minAvg = avg3;
                 minIdx = i;
             }
         }
 
+        double avg2 = (A[N-2] + A[N-1]) / 2d;
+        if (avg2 < minAvg) {
+            minIdx = N-2;
+        }
+
         return minIdx;
-    }*/
-
-    public int solution(int[] A) {
-
-        return -1;
     }
 
     public int solutionNaive(int[] A) {
@@ -61,7 +55,6 @@ public class MinAvgTwoSlice {
             }
         }
 
-
         return minIdx;
     }
 
@@ -72,5 +65,8 @@ public class MinAvgTwoSlice {
 
         final int[] B = { 1000, -1000 };
         Assert.assertEquals(0, solution(B));
+
+        final int[] C = { -3, -5, -8, -4, -10 };
+        Assert.assertEquals(2, solution(C));
     }
 }
--- src/main/java/sieveOfEratosthenes/CountSemiprimes.java
+++ src/main/java/sieveOfEratosthenes/CountSemiprimes.java
@@ -7,9 +7,10 @@ import java.util.Arrays;
 
 public class CountSemiprimes {
 
-    // 
+    //
     public int[] solution(int N, int[] P, int[] Q) {
-        //TODO
+
+
         return null;
     }
 
