76710b9f835085b5dad44a7ebc19caaec47b295c
bmh10 <bmhomer13@gmail.com>
2016-03-05 11:40:19 +0000

    CommonPrimeDivisors complete

--- src/main/java/euclideanAlgorithm/CommonPrimeDivisors.java
+++ src/main/java/euclideanAlgorithm/CommonPrimeDivisors.java
@@ -3,19 +3,16 @@ package euclideanAlgorithm;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class CommonPrimeDivisors {
 
+    // Time: O(Z * log(max(A) + max(B))**2) Space: O(1)
     public int solution(int[] A, int[] B) {
         final int Z = A.length;
 
         int sameCount = 0;
         for (int i = 0; i < Z; i++) {
-            Set<Integer> primeDivsA = findPrimeDivisors(A[i]);
-            Set<Integer> primeDivsB = findPrimeDivisors(B[i]);
-            if (primeDivsA.equals(primeDivsB)) {
+            int gcd = gcd(A[i], B[i]);
+            if (check(A[i], gcd) && check(B[i], gcd)) {
                 sameCount++;
             }
         }
@@ -23,43 +20,33 @@ public class CommonPrimeDivisors {
         return sameCount;
     }
 
-    private Set<Integer> findPrimeDivisors(int n) {
-        Set<Integer> primeDivisors = new HashSet<>();
-        for (int i = 1; i*i <= n; i++) {
-            if (n % i == 0) {
-                if (isPrime(i)) {
-                    primeDivisors.add(i);
-                }
-
-                int matchingDivisor = n / i;
-                if (isPrime(matchingDivisor)) {
-                    primeDivisors.add(matchingDivisor);
-                }
-            }
-        }
-
-        return primeDivisors;
-    }
-
-    // Use sieve to generate all in advance
-    private boolean isPrime(int n) {
-        for (int i = 2; i*i <= n; i++) {
-            if (n % i == 0) {
+    // Checks if all prime divisors of n can be found
+    // in prime divisors of gcd(a,b)
+    private boolean check(int n, int gcd) {
+        int rest = n / gcd;
+        while (gcd % rest != 0) {
+            int gcd_tmp = gcd(gcd, rest);
+            if (gcd_tmp == 1){
                 return false;
             }
+
+            rest /= gcd_tmp;
         }
 
         return true;
     }
 
+    private int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+
+        return gcd(b, a % b);
+    }
+
+
     @Test
     public void test() {
-        Assert.assertTrue(isPrime(1));
-        Assert.assertTrue(isPrime(2));
-        Assert.assertFalse(isPrime(4));
-        Assert.assertTrue(isPrime(5));
-        Assert.assertTrue(isPrime(17));
-
         int[] A = { 15, 10, 3 };
         int[] B = { 75, 30, 5 };
         Assert.assertEquals(1, solution(A, B));
